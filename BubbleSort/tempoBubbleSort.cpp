58572908b2cd1418759999253895e71b762505e3
Warlike <warlikerichard@hotmail.com>
2024-11-07 23:21:30 -0300

    w/ chrono and video

--- BubbleSort/tempoBubbleSort.cpp
+++ BubbleSort/tempoBubbleSort.cpp
@@ -2,15 +2,18 @@
 #include "bubbleSort.hpp"
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 // g++ ./BubbleSort/tempoBubbleSort.cpp ./BubbleSort/bubbleSortIterativo.cpp ./BubbleSort/bubbleSortRecursivo.cpp ./RNG/list_RNG.cpp -o q2.5 -g -W
 
 /* Função main para transformar a lista em vetor, chamar o algoritmo de ordenação
    e testar o tempo de execução do algoritmo */ 
 int main() {
-    
+    // Captura o tempo de início
+    auto start = std::chrono::high_resolution_clock::now();
+
     // Altere a lista a ser ordenada aqui
-    std::string fileName = "./listas/lista_100.txt";
+    std::string fileName = "./listas/lista_10000.txt";
     std::ifstream list;
     list.open(fileName, std::ios::in);
 
@@ -22,10 +25,19 @@ int main() {
 
     listToVector(arr, fileName, n);
     
-    // bubbleSort(arr, n);
+    //bubbleSort(arr, n);
     bubbleSortR(arr, n);
 
     vectorToList(arr, n);
+
+    // Captura o tempo de término
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // Calcula a duração em milissegundos
+    std::chrono::duration<double, std::micro> duration = end - start;
+
+    // Exibe o tempo de execução
+    std::cout << "O código levou " << duration.count() << " microssegundos para rodar." << std::endl;
 
     return 0;
 }
--- MergeSort/tempoMergeSort.cpp
+++ MergeSort/tempoMergeSort.cpp
@@ -2,14 +2,17 @@
 #include "mergeSort.hpp"
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 // g++ ./MergeSort/tempoMergeSort.cpp ./MergeSort/mergeSortIterativo.cpp ./MergeSort/funcaoMerge.cpp ./MergeSort/mergeSortRecursivo.cpp ./RNG/list_RNG.cpp -o q2.5 -g -W
 
 // Função main para transformar a lista em vetor e chamar o algoritmo de ordenação
 int main() {
-    
+    // Captura o tempo de início
+    auto start = std::chrono::high_resolution_clock::now();
+
     // Altere a lista a ser ordenada aqui
-    std::string fileName = "./listas/lista_100.txt";
+    std::string fileName = "./listas/lista_10000.txt";
     std::ifstream list;
     list.open(fileName, std::ios::in);
 
@@ -21,10 +24,19 @@ int main() {
 
     listToVector(arr, fileName, n);
     
-    // iterativeMergeSort(n, arr);
-    recursiveMergeSort(n, arr, 0, n - 1);
+    iterativeMergeSort(n, arr);
+    //recursiveMergeSort(n, arr, 0, n - 1);
 
     vectorToList(arr, n);
+
+    // Captura o tempo de término
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // Calcula a duração em milissegundos
+    std::chrono::duration<double, std::micro> duration = end - start;
+
+    // Exibe o tempo de execução
+    std::cout << "O código levou " << duration.count() << " microssegundos para rodar." << std::endl;
 
     return 0;
 }
